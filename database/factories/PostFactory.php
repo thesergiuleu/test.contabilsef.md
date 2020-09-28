af9bdc8fec4df4d8ac33108353613e102b5b7991
Stefan <hincustefan1995@gmail.com>
2020-09-28 18:07:16 +0300

    test

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -11,6 +11,7 @@ $factory->define(Post::class, function (Faker $faker) {
         'title' => $faker->sentence,
         'seo_title' => $faker->sentence,
         'body' => $faker->text,
+        'privacy' => $faker->boolean,
         'image' => 'uploads/' . $faker->image('storage/app/public/uploads',640,480, null, false),
         'slug' => $faker->slug,
         'meta_description' => $faker->sentence,
