7586a140b8355cf02f1bce3b68444b1318de1e2b
Stefan <hincustefan1995@gmail.com>
2020-10-06 21:44:02 +0300

    fix

--- resources/views/layouts/common/thumbnail.blade.php
+++ resources/views/layouts/common/thumbnail.blade.php
@@ -1,5 +1,4 @@
-<div class="img-responsive-block">
-    <a href="{{ $item->post_url }}" style="position: absolute; z-index: 1;top:0;left: 0;height: 100%;width: 100%"></a>
+<a href="{{ $item->post_url }}" class="textP">
     {!! $item->new_on_pic !!}
-    <img class="img-left responsive" src="{{ $item->thumbnail_url }}" alt="">
-</div>
+    <img class="img-left responsive" style="max-width: 200px" src="{{ $item->thumbnail_url }}" alt="">
+</a>
