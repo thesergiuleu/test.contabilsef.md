52c9493e9f73c7453bec9f3dce11117b371229df
Stefan <hincustefan1995@gmail.com>
2020-10-14 20:03:01 +0300

    push

--- resources/views/layouts/common/link.blade.php
+++ resources/views/layouts/common/link.blade.php
@@ -1,6 +1,6 @@
 @if( $item->link )
     <p class="servici-v" style="display: -webkit-inline-box;"><img style="width: 12px; height: 11px;" src="{{ asset('assets/imgs/cl.png') }}" alt="">
-        {!! $item->link !!}
+        {!! preg_replace("/<a(.*?)>/", "<a$1 target=\"_blank\">", $item->link); !!}
     </p>
 @endif
 
