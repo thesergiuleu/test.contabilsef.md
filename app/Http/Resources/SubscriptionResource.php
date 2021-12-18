4e965fc5a9ee9d7a757a9278f886f47b46de8c82
thesergiuleu <sergiu.leu@smartdata.solutions>
2021-12-18 17:27:14 +0200

    [tasd]

---
+++ app/Http/Resources/SubscriptionResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SubscriptionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->resource->id,
+            'package' => $this->resource->package,
+            'payment' => $this->resource->payment,
+            'service' => $this->resource->service,
+            'status' => $this->resource->status(),
+            'user' => $this->resource->user,
+        ];
+    }
+}
