445c070edbfca4a8be5f13434b7e01aa06957170
thesergiuleu <sergiu.leu@smartdata.solutions>
2021-12-19 11:39:10 +0200

    asd

---
+++ app/Console/Commands/SyncDatabase.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SyncDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync:copy_db';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->alert(env('DB_STAGING_USERNAME'));
+        $this->alert(env('DB_STAGING_PASSWORD'));
+        $this->alert(env('DB_STAGING_DATABASE'));
+        $this->alert("mysqldump --column-statistics=0 -P 3306 -h " . env('DB_STAGING_HOST') . " -u" . env('DB_STAGING_USERNAME') . " -p" . env('DB_STAGING_PASSWORD') . ' ' . env('DB_STAGING_DATABASE') . ' > full_db.sql');
+
+        exec("mysqldump --column-statistics=0 -P 3306 -h " . env('DB_STAGING_HOST') . " -u" . env('DB_STAGING_USERNAME') . " -p" . env('DB_STAGING_PASSWORD') . ' ' . env('DB_STAGING_DATABASE') . ' > full_db.sql');
+
+        $this->alert('DUMP from staging done.');
+
+        $this->alert(env('DB_USERNAME'));
+        $this->alert(env('DB_PASSWORD'));
+        $this->alert('contabilsef_md');
+        $this->alert('mysql -u' . env('DB_USERNAME') . ' -p' . env('DB_PASSWORD') . ' ' . 'contabilsef_md' . ' < full_db.sql');
+
+        exec('mysql -u' . env('DB_USERNAME') . ' -p' . env('DB_PASSWORD') . ' ' . 'contabilsef_md' . ' < full_db.sql');
+
+        $this->alert('IMPORT to local db done');
+    }
+}
