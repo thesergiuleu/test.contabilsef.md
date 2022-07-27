541dd8f2e5997c9a764d1eabb0f2787a06694d75
sergiu.leu <sergiu.leu@smartdata.solutions>
2022-07-27 10:42:31 +0300

    lol

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => 'contabilsef_md',
+            'database' => env('DB_DATABASE', 'contabilsef_md'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
